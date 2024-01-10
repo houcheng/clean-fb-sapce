f9ab8840a8bdfa876fcbc7b8f3f2eb34cc641efb
Houcheng Lin <houcheng@gmail.com>
2024-01-10 11:18:02 +0800

    Add some buttons

--- src/cleanFb.user.js
+++ src/cleanFb.user.js
@@ -9,6 +9,24 @@
 // @downloadURL  https://github.com/houcheng/CleanFBSapce/raw/master/src/cleanFb.user.js
 // ==/UserScript==
 
+
+var deletedTitles = [];
+function displayDeletedTitles() {
+    // Create a modal or div to display the titles
+    const modal = document.createElement('div');
+    modal.style = 'position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: white; padding: 20px; border: 1px solid black; z-index: 2000; max-height: 300px; overflow-y: scroll;';
+    modal.innerHTML = '<h2>Deleted Titles</h2><ul>' + deletedTitles.map(title => `<li>${title}</li>`).join('') + '</ul>';
+    document.body.appendChild(modal);
+
+    // Click outside the modal to close
+    modal.onclick = function(event) {
+        if (event.target === modal) {
+            document.body.removeChild(modal);
+        }
+    };
+}
+
+
 function createBannerNode() {
     const node = document.createElement ('div');
     node.innerHTML = '';
@@ -36,17 +54,46 @@ function createBannerNode() {
             background:             white;
         }
     ` );
+    const titleNode = document.createElement('div');
+    node.appendChild(titleNode);
+
+    // Button to show deleted titles
+    const showTitlesBtn = document.createElement('button');
+    showTitlesBtn.innerHTML = 'Show';
+    showTitlesBtn.onclick = () => {
+        displayDeletedTitles();
+    };
+    node.appendChild(showTitlesBtn);
+
+    // Button to show deleted titles
+    const debugBtn = document.createElement('button');
+    debugBtn.innerHTML = 'Debug';
+    debugBtn.onclick = () => {
+        debugNode = true;
+    };
+    node.appendChild(debugBtn);
+
+    // Button to hide
+    const hideBtn = document.createElement('button');
+    hideBtn.innerHTML = 'Hide';
+    hideBtn.onclick = () => {
+        node.style.display = 'none'; // Hide the entire banner
+    };
+    node.appendChild(hideBtn);
+
     return node;
 }
 
 const bannerNode = createBannerNode();
 const CheckInterval = 3000;
-const NeedToRemoveKeywords = ['為你推薦', 'Suggested for you', '贊助', 'Sponsored', 'Reels and short videos'];
+const NeedToRemoveKeywords = ['為你推薦', 'Suggested for you', '贊助', 'Sponsored', 'Reels and short videos', 'Follow'];
 
 var lastRunTick = (new Date()).getTime();
 var removedCount = 0;
+var debugNode = false;
 
 function checkKeywordsExist(node) {
+    if (debugNode) console.log("inner html", node.innerHTML);
     if (!node.innerHTML) return false;
     return NeedToRemoveKeywords.some((lang) => node.innerHTML.contains('dir="auto">' + lang + '</span>')) || NeedToRemoveKeywords.some((lang) => node.innerHTML.contains('">' + lang + '<')) ;
 }
@@ -77,10 +124,14 @@ function removeRecommandPost() {
         if (shouldRemove) {
             removedCount += 1;
             const msg = node.innerText ? node.innerText.split('\n')[0] : "no-name";
-            bannerNode.innerHTML = `<div>${removedCount} ${msg}</div>`;
+            // bannerNode.innerHTML = `<div>${removedCount} ${msg}</div>`;
+            console.log(`<div>${removedCount} ${msg}</div>`)
+            deletedTitles.push(`${removedCount} ${msg}`);
+            bannerNode.childNodes[0].innerHTML = `<div>${removedCount} ${msg}</div>`;
             node.remove();
         }
     });
+    if (debugNode) debugNode = false;
     lastRunTick = nowTick;
 }
 
